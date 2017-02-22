2ab3fe95d8d232c673d3b68005d067faa493c2cc
Siyuan Liu <weifengzi2009@gmail.com>
2017-02-22 23:44:42 +0800

    use actual file instead of symlink

--- compilers/GCC/4.9.4-2.25.lua
+++ compilers/GCC/4.9.4-2.25.lua
@@ -1,1 +1,23 @@
-/opt/apps/modules/all/GCC/4.9.4-2.25.lua
+help([[The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Java, and Ada,
+ as well as libraries for these languages (libstdc++, libgcj,...). - Homepage: http://gcc.gnu.org/]])
+
+whatis([[Description: The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Java, and Ada,
+ as well as libraries for these languages (libstdc++, libgcj,...). - Homepage: http://gcc.gnu.org/]])
+
+local root = "/opt/apps/software/GCC/4.9.4-2.25"
+
+conflict("GCC")
+
+if not isloaded("GCCcore/4.9.4") then
+    load("GCCcore/4.9.4")
+end
+
+if not isloaded("binutils/2.25-GCCcore-4.9.4") then
+    load("binutils/2.25-GCCcore-4.9.4")
+end
+
+setenv("EBROOTGCC", "/opt/apps/software/GCCcore/4.9.4")
+setenv("EBVERSIONGCC", "4.9.4")
+setenv("EBDEVELGCC", pathJoin(root, "easybuild/GCC-4.9.4-2.25-easybuild-devel"))
+
+-- Built with EasyBuild version 3.1.0
--- compilers/GCC/5.4.0-2.26.lua
+++ compilers/GCC/5.4.0-2.26.lua
@@ -1,1 +1,23 @@
-/opt/apps/modules/all/GCC/5.4.0-2.26.lua
+help([[The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Java, and Ada,
+ as well as libraries for these languages (libstdc++, libgcj,...). - Homepage: http://gcc.gnu.org/]])
+
+whatis([[Description: The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Java, and Ada,
+ as well as libraries for these languages (libstdc++, libgcj,...). - Homepage: http://gcc.gnu.org/]])
+
+local root = "/opt/apps/software/GCC/5.4.0-2.26"
+
+conflict("GCC")
+
+if not isloaded("GCCcore/5.4.0") then
+    load("GCCcore/5.4.0")
+end
+
+if not isloaded("binutils/2.26-GCCcore-5.4.0") then
+    load("binutils/2.26-GCCcore-5.4.0")
+end
+
+setenv("EBROOTGCC", "/opt/apps/software/GCCcore/5.4.0")
+setenv("EBVERSIONGCC", "5.4.0")
+setenv("EBDEVELGCC", pathJoin(root, "easybuild/GCC-5.4.0-2.26-easybuild-devel"))
+
+-- Built with EasyBuild version 3.1.0
