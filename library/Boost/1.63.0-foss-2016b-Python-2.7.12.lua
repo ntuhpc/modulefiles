bb319f547529b4ed42f6513dd15b775979198ff3
Siyuan Liu <weifengzi2009@gmail.com>
2017-03-23 16:55:28 +0800

    add Boost

---
+++ library/Boost/1.63.0-foss-2016b-Python-2.7.12.lua
@@ -0,0 +1,33 @@
+help([[Boost provides free peer-reviewed portable C++ source libraries. - Homepage: http://www.boost.org/]])
+
+whatis([[Description: Boost provides free peer-reviewed portable C++ source libraries. - Homepage: http://www.boost.org/]])
+
+local root = "/opt/apps/software/Boost/1.63.0-foss-2016b-Python-2.7.12"
+
+conflict("Boost")
+
+if not isloaded("foss/2016b") then
+    load("foss/2016b")
+end
+
+if not isloaded("bzip2/1.0.6-foss-2016b") then
+    load("bzip2/1.0.6-foss-2016b")
+end
+
+if not isloaded("zlib/1.2.8-foss-2016b") then
+    load("zlib/1.2.8-foss-2016b")
+end
+
+if not isloaded("Python/2.7.12-foss-2016b") then
+    load("Python/2.7.12-foss-2016b")
+end
+
+prepend_path("CPATH", pathJoin(root, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(root, "lib"))
+prepend_path("LIBRARY_PATH", pathJoin(root, "lib"))
+setenv("EBROOTBOOST", root)
+setenv("EBVERSIONBOOST", "1.63.0")
+setenv("EBDEVELBOOST", pathJoin(root, "easybuild/Boost-1.63.0-foss-2016b-Python-2.7.12-easybuild-devel"))
+
+setenv("BOOST_ROOT", "/opt/apps/software/Boost/1.63.0-foss-2016b-Python-2.7.12")
+-- Built with EasyBuild version 3.1.0
